c69952eada2ef53f63a73fa12a9f5e325a52e380
eagleos <www@xmyy.com>
2026-06-24 23:26:34 +0800

    update dev-tools

--- tools/assets/json-minify-1f983b62.js
+++
@@ -1,5 +0,0 @@
-import{_ as a}from"./FormatTransformer.vue_vue_type_script_setup_true_lang-7216d51e.js";import{l as r}from"./index-231c3e11.js";import{w as i}from"./defaults-4d6daddf.js";import{d as s,o as l,c as p}from"./index-fbb35438.js";import"./TextareaCopyable-6b1fc640.js";import"./Copy-d79f4e61.js";import"./Scrollbar-b1465fcb.js";const u=`{
-	"hello": [
-		"world"
-	]
-}`,N=s({__name:"json-minify",setup(m){const t=o=>i(()=>JSON.stringify(r.parse(o),null,0),""),e=[{validator:o=>o===""||r.parse(o),message:"Provided JSON is not valid."}];return(o,f)=>{const n=a;return l(),p(n,{"input-label":"Your raw JSON","input-default":u,"input-placeholder":"Paste your raw JSON here...","output-label":"Minified version of your JSON","output-language":"json","input-validation-rules":e,transformer:t})}}});export{N as default};
